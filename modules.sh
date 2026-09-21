ce5848747f9cad62fd2679f0ea8baa7be510b9da
Stas Stavnichuk <1835287+1mr@users.noreply.github.com>
2026-09-21 09:46:26 +0300

    feat: debian11,debian12,debian13 bulds

--- modules.sh
+++ modules.sh
@@ -5,16 +5,20 @@ set -e
 NGINX_VERSION="${NGINX_VERSION:-1.30.5}"
 PLATFORMS="${PLATFORMS:-linux/amd64}"
 # PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64}"
+DEBIAN_VERSIONS="${DEBIAN_VERSIONS:-11,12,13}"
 
 for p in $(echo "${PLATFORMS}" | tr ',' ' '); do
   arch=$(echo "${p}" | tr '/' '-')
-  for m in ngx_brotli ngx_geoip2 ngx_vts; do
-    docker buildx build \
-      --platform "${p}" \
-      --build-arg NGINX_VERSION="${NGINX_VERSION}" \
-      --build-arg TARGETARCH="${p#linux/}" \
-      --output "type=local,dest=./modules/${arch}" \
-      -f "${m}/Dockerfile" \
-      .
+  for d in $(echo "${DEBIAN_VERSIONS}" | tr ',' ' '); do
+    for m in ngx_brotli ngx_geoip2 ngx_vts; do
+      docker buildx build \
+        --platform "${p}" \
+        --build-arg NGINX_VERSION="${NGINX_VERSION}" \
+        --build-arg DEBIAN_VERSION="${d}" \
+        --build-arg TARGETARCH="${p#linux/}" \
+        --output "type=local,dest=./modules/${arch}" \
+        -f "${m}/Dockerfile" \
+        .
+    done
   done
 done
